70fe8b604d2662eb785dcfe92ff7af41cf3fcb2d
EStherKiguru <esipraisewambui@gmail.com>
2024-08-22 15:03:15 +0300

    Making change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module for the make change problem using dynamic programming"""
+
+
+def makeChange(coins, total):
+    """Using the bottom-up method"""
+    if total <= 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for amt in range(1, total + 1):
+        for coin in coins:
+            if coin <= amt:
+                dp[amt] = min(dp[amt], dp[amt - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
---
+++ 0x08-making_change/README.md
@@ -0,0 +1,3 @@
+#0x08. Making Change 
+
+This project invloves solving the coin change probelm using dynamic programming or greedy algorithms
